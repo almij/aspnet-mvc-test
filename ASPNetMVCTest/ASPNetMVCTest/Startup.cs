e95659a5acf2a5d36ca85af730f2339e765f7273
almik <alexmikhylov@gmail.com>
2019-10-05 20:40:34 +0300

    test

--- ASPNetMVCTest/ASPNetMVCTest/Startup.cs
+++ ASPNetMVCTest/ASPNetMVCTest/Startup.cs
@@ -39,12 +39,6 @@ namespace ASPNetMVCTest
             //dbUrl = @"postgres://dkxuqkiskbvith:7fb1d50ba36790282e72fedd4e98d0f5621971b0927603c7b66b5df1f0001243@ec2-54-217-235-87.eu-west-1.compute.amazonaws.com:5432/d782d2amg07hcs";
 
             bool isUrl = Uri.TryCreate(dbUrl, UriKind.Absolute, out Uri url);
-
-            Console.WriteLine("Host: " + url.Host);
-            Console.WriteLine("Port: " + url.Port);
-            Console.WriteLine("Database: " + url.LocalPath.Substring(1));
-            Console.WriteLine("Username: " + url.UserInfo.Split(':')[0]);
-            Console.WriteLine("Password: " + url.UserInfo.Split(':')[1]);
             var connectionString = $"host={url.Host};username={url.UserInfo.Split(':')[0]};password={url.UserInfo.Split(':')[1]};database={url.LocalPath.Substring(1)};pooling=true;SSL Mode=Require;TrustServerCertificate=True;";
             services.AddEntityFrameworkNpgsql()
                 .AddDbContext<ApplicationDbContext>(options => options.UseNpgsql(connectionString));
